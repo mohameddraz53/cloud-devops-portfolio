a22fc26ea757f912b580974a4a6dbbd77e21d371
Mohamed Ibrahim Lotfy Draz <drazmohamed542@gmail.com>
2025-12-30 22:13:33 +0200

    vpc -Peering connection between two virtual vpc

---
+++ file.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+git add /aws cloud architect--DPI Gradution project
+git commit -m "vpc -Peering connection between two virtual vpc"
+
+git add \Badges
+git commit -m "Badges I've Achieved On aws Academy and Mahara_Tech"
+
+git add \Badges\Aws Acdemy Badges
+git commit -m "Aws academy badges (cloud foundation-cloud operations - cloud Architect)"
+
+git add \Badges\Mahara-Tech Badges
+git commit -m "Mahara-Tech Badges (DevOps-Prerequests)"
+
+git add \Devops mini  Project ( In progress ... )
+git commit -m "Devops mini  Project  (Bash -->linux admin based skill - Docker)"
+
+git add \Devops mini  Project ( In progress ... )\bash mini project
+git commit -m "Devops mini  Project  (Bash Projects)"
+
+git add \Devops mini  Project ( In progress ... )\Docker -enginx  Mini Project\myproject-main
+git commit -m "Docker Project (hosting website by docker container on enginx server )"
+
+git add \My Cetificate
+git commit -m "my Certificates in (Delta Universty and Dpi (aws cloud solution) - Dpi Business English - English (MU) certificate)"
+
+git add \My Cetificate\DPI-Aws-Architect cert
+git commit -m "Cetificate\DPI-Aws-Architect cert"
+
+git add \My Cetificate\Dpi-Buisness-English cert
+git commit -m "Dpi-Buisness-English cert"
+
+git add \My Cetificate\English Course (MU) cert
+git commit -m "english (MU) Course Cert -->B1"
+
+git add \My Cetificate\Gradution cert
+git commit -m "Delta Universty Gradution Certtifcate"
+
+git add \My CV
+git commit -m "this My Cv"
